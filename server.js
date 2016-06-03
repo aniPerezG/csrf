c3edabd35b223c4c46f5be58f2815d306170e953
Ana Perez Ghiglia <ana.ghiglia@despegar.com>
2016-06-03 00:43:38 -0300

    add login to html

--- server.js
+++ server.js
@@ -22,36 +22,3 @@ var response = fs.readFile('./index.html',  function (err, html) {
 	});
 });
 
-
-
-// var dispatcher = require('httpdispatcher');
-
-
-//We need a function which handles requests and send response
-// function handleRequest(request, response){
-// 	response.writeHead(200);
-// 	response.write(response);
-//     response.end();
-// }
-
-
-
-// function handleRequest(request, response){
-//     try {
-//         //log the request on console
-//         console.log(request.url);
-//         //Disptach
-//         dispatcher.dispatch(request, response);
-//     } catch(err) {
-//         console.log(err);
-//     }
-// }
-
-//Create a server
-// var server = http.createServer(handleRequest);
-
-//Lets start our server
-// server.listen(PORT, function(){
-//     //Callback triggered when server is successfully listening. Hurray!
-//     console.log("Server listening on: http://localhost:%s", PORT);
-// });
